af77c29d8b8ae420646c6ded7ac112505ac9b3d8
Fede_B <83786463+fedeb123@users.noreply.github.com>
2024-06-29 15:32:10 -0300

    Renombrado de numero afiliado a numero de afiliacion que seria el numero identificador propio de la obra social

--- src/Laboratorio/ObraSocial.java
+++ src/Laboratorio/ObraSocial.java
@@ -4,23 +4,23 @@ import DTOs.ObraSocialDTO;
 
 public class ObraSocial {
     private String obraSocial;
-    private int numeroAfiliado;
+    private int numeroAfilicion;
 
-    public ObraSocial(String obraSocial, int numeroAfiliado) {
+    public ObraSocial(String obraSocial, int numeroAfilicion) {
         this.obraSocial = obraSocial;
-        this.numeroAfiliado = numeroAfiliado;
+        this.numeroAfilicion = numeroAfilicion;
     }
 
     public String getObraSocial() {
         return obraSocial;
     }
 
-    public int getNumeroAfiliado() {
-        return numeroAfiliado;
+    public int getNumeroAfilicion() {
+        return numeroAfilicion;
     }
 
     public ObraSocialDTO toDTO(){
-        ObraSocialDTO obraSocialDTO = new ObraSocialDTO(this.obraSocial, this.numeroAfiliado);
+        ObraSocialDTO obraSocialDTO = new ObraSocialDTO(this.obraSocial, this.numeroAfilicion);
         return obraSocialDTO;
     }
 }
