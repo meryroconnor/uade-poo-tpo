32c0a0b9652038e3bf0005e7b267ab6b24172a76
Fede_B <83786463+fedeb123@users.noreply.github.com>
2024-07-05 00:38:30 -0300

    Reemplazo del viejo parametro numero de afiliado (que en todo caso pertenece al paciente) por uno nuevo que sea el ID de la obra social

--- src/DTOs/ObraSocialDTO.java
+++ src/DTOs/ObraSocialDTO.java
@@ -1,16 +1,21 @@
 package DTOs;
 
 public class ObraSocialDTO {
-    private int numeroAfiliado;
+    private int obraSocialID;
     private String obraSocial;
 
 
-    public ObraSocialDTO(String obraSocial, int numeroAfiliado){
-        this.numeroAfiliado = numeroAfiliado;
+    public ObraSocialDTO(String obraSocial, int obraSocialID){
+        this.obraSocialID = obraSocialID;
         this.obraSocial = obraSocial;
     }
 
-    public int getNumeroAfiliado() {
-        return numeroAfiliado;
+    public int getObraSocialID() {
+        return obraSocialID;
+    }
+
+    public String getObraSocial() {
+        return obraSocial;
     }
 }
+
